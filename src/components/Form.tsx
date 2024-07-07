f469ce74fdb75b616a4c0d7dd90b961ef7cdb061
A.Yoshioka <akihiko.yoshioka@pro-spire.co.jp>
2024-07-07 23:38:59 +0900

    init deploy

---
+++ src/components/Form.tsx
@@ -0,0 +1,16 @@
+type FormProps = {
+    setCity:React.Dispatch<React.SetStateAction<string>>
+    getWeather:(e: React.FormEvent<HTMLElement>) => void
+    city:string
+}
+
+const Form = (props:FormProps) => {
+    return (
+        <form onSubmit={props.getWeather}>
+            <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)} value={props.city}/>
+            <button type="submit">Get Weather</button>
+        </form>
+    )
+}
+
+export default Form
---
+++ src/components/Title.tsx
@@ -0,0 +1,3 @@
+const Title = () => <h1>ReactA World Weather</h1>
+
+export default Title
